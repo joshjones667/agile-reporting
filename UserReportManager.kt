0b09ca527e55de1dc18d9b9cc7b28668bbf440f6
ahmedgreen668 <44838908+ahmedgreen668@users.noreply.github.com>
2018-11-07 05:00:10 -0800

    Added the User Reporting Logic

--- UserReportManager.kt
+++ UserReportManager.kt
@@ -1,3 +1,13 @@
 object UserReportManager {
 
+    val userReportService: UserReportService = UserReportService()
+
+    fun sendAllUsersReports(users: List<User>) {
+        for (user in users) {
+            for (userReport in user.reports.orEmpty()) {
+                userReportService.sendReport(userReport)
+            }
+        }
+    }
+
 }
